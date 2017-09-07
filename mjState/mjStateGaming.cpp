0ae54ad15535d97e47d93968b0dd7e8bc5b7fa6c
zerak <Kingbug2010@126.com>
2017-09-07 18:23:17 +0800

    log print

--- mjState/mjStateGaming.cpp
+++ mjState/mjStateGaming.cpp
@@ -7,15 +7,15 @@ MjStateGaming::MjStateGaming()
 
 void MjStateGaming::enter()
 {
-
+    std::cout<< "state gaming enter" << std::endl;
 }
 
 void MjStateGaming::exit()
 {
-
+    std::cout<< "state gaming exit" << std::endl;
 }
 
 void MjStateGaming::update()
 {
-
+    std::cout<< "state gaming update" << std::endl;
 }
