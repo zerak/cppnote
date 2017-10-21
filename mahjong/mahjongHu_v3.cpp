f8f6b949b6bd9d38418ea6ecac2d74db75b4c6c6
zerak <Kingbug2010@126.com>
2017-10-21 14:15:00 +0800

    mahjong version3 add hun support

--- mahjong/mahjongHu_v3.cpp
+++ mahjong/mahjongHu_v3.cpp
@@ -13,13 +13,9 @@
 #include <numeric>  // accumulate
 using namespace std;
 
-/*
- * 该版本在v2的基础上，在生成带混表时，考虑杠的情况，
- * 比如风字牌：4000000,在有一个混儿的情况下可胡
-*/
-class MjM
 class MjMapTbV3 {
 private:
+public: // todo
 #define MAX_HUN_COUNT 8
 	typedef int Key;
 	typedef int Val;
@@ -32,18 +28,19 @@ public:
 
 public:
 	void load() {
-		if (!loadTb(SanSe, 0)) {
+//		if (!loadTb(SanSe, 0)) {
 			cout << "gen map" << endl;
-			//genSanSeJiang();
+			genSanSeJiang();
 			genSanSeKe();       // 生成三色牌Ke
 			genSanSeShun();     // 生成三色牌ShunZi
-			//genFengJiang();
+			genFengJiang();
 			genFengKe();        // 生成风字牌Ke
+			genFengGang();
 
 			genSanSeMap();
 			genFengMap();
 			return;
-		}
+//		}
 
 		//cout << "sanseKe:" << m_dSanSeKe.size() << endl;
 		//cout << "sanseShun:" << m_dSanSeShun.size() << endl;
@@ -57,13 +54,23 @@ public:
 		for (int j = 0; j < sizeof(m_dFinalFengHun) / sizeof(m_dFinalFengHun[0]); ++j) {
 			loadTb(FengZi, j);
 		}
+
+		// load jiang + ke/shun
+		for (int i = 0; i < sizeof(m_dFinalJiangSanSeHun) / sizeof(m_dFinalJiangSanSeHun[0]); ++i) {
+			loadTb(SanSe, i);
+		}
+		// load feng
+		for (int j = 0; j < sizeof(m_dFinalJiangFengHun) / sizeof(m_dFinalJiangFengHun[0]); ++j) {
+			loadTb(FengZi, j);
+		}
 	}
 
 	// handList: 当前手牌
 	// len: handList长度
 	// mjCcodeIdx: 最后拿的牌index
 	// hunCount: 混个数
-	bool checkHu(int *handList, int len, int hunCount) {
+	bool checkHu(int *handList, int len) {
+		int hunCount = handList[0];
 		// check 3n2
 //        if (!is3N2(handList, len, hunCount)) {
 //            return false;
@@ -101,6 +108,7 @@ public:
 	}
 
 private:
+public: // todo
 	enum MjType {
 		SanSe = 0,
 		FengZi = 1
@@ -117,12 +125,20 @@ private:
 		return Val(0);
 	}
 
-	tbMap& getMap(MjType tpe, int hunCount) {
-		if (tpe == SanSe) {
-			return m_dFinalSanSeHun[hunCount];
+	tbMap& getMap(MjType tpe, int hunCount, bool hasJiang = false) {
+		if (hasJiang) {
+			if (tpe == SanSe) {
+				return m_dFinalJiangSanSeHun[hunCount];
+			} else {
+				return m_dFinalJiangFengHun[hunCount];
+			}
+		} else {
+			if (tpe == SanSe) {
+				return m_dFinalSanSeHun[hunCount];
+			} else {
+				return m_dFinalFengHun[hunCount];
+			}
 		}
-
-		return m_dFinalFengHun[hunCount];
 	}
 
 	void add(tbMap &tm, Key &key, Val &val) {
@@ -141,9 +157,13 @@ private:
 		return "feng" + itos(hunCount);
 	}
 
-	bool loadTb(MjType tpe, int hunCount) {
+	bool loadTb(MjType tpe, int hunCount, bool hasJiang = false) {
 		char name[256];
-		sprintf(name, "tbl/table_%s.tbl", getTbName(tpe, hunCount).c_str());
+		if (hasJiang) {
+			sprintf(name, "tbl/table_jiang_%s.tbl", getTbName(tpe, hunCount).c_str());
+		} else {
+			sprintf(name, "tbl/table_%s.tbl", getTbName(tpe, hunCount).c_str());
+		}
 		FILE *fp = fopen(name, "rb");
 		if (fp == NULL) {
 			cout << "can't load table:" << name << endl;
@@ -154,7 +174,7 @@ private:
 		int key = 0;
 		while (fscanf(fp, "%d\n", &key) != EOF) {
 			Val val = getVal();
-			add(getMap(tpe, hunCount), key, val);
+			add(getMap(tpe, hunCount, hasJiang), key, val);
 			num++;
 		}
 		cout << "load table:" << name << " " << num << "times, size:" << getMap(tpe, hunCount).size() << endl;
@@ -240,12 +260,13 @@ private:
 	tbVec getKey(Key key) {
 		tbVec vec;
 		char buf[9];
+		memset(buf, 0, sizeof(buf));
 		sprintf(buf, "%d", key);
 		char tmp[9];
-		memset(tmp, 0, sizeof(tmp));
 		for (int i = 0; i < sizeof(buf); ++i) {
 			if (buf[i] - '0' > 0){
 				buf[i]--;
+				memset(tmp, 0, sizeof(tmp));
 				memcpy(tmp, buf, sizeof(tmp));
 				int num = atoi(tmp);
 				if (num>0) {
@@ -325,6 +346,18 @@ private:
 		}
 		dumpTest(m_dFengKe, "fengKe");
 		return m_dFengKe;
+	}
+	tbVec& genFengGang() {
+		int title[7];
+		memset(title, 0, sizeof(title));
+		for (int i = 0; i < 7; ++i) {
+			title[i] = 4;
+			Key key = getKey(title, 7);
+			m_dFengGang.push_back(key);
+			memset(title, 0, sizeof(title));
+		}
+		dumpTest(m_dFengGang, "fengGang");
+		return m_dFengGang;
 	}
 	void dumpTest(tbVec &d, string tpe) {
 		char name[256];
@@ -446,11 +479,11 @@ private:
 		chooseFrom(m_dSanSeShun, tb, 4, m_dSanSeShun.size());
 	}
 	void genSanSeMap() {
+        // 刻、顺
 		genSanSe1(m_dFinalSanSeHun[0]);
 		genSanSe2(m_dFinalSanSeHun[0]);
 		genSanSe3(m_dFinalSanSeHun[0]);
 		genSanSe4(m_dFinalSanSeHun[0]);
-
 		// hun
 		for (int hunCount = 0; hunCount < sizeof(m_dFinalSanSeHun) / sizeof(m_dFinalSanSeHun[0]) -1; ++hunCount) {
 			//cout << "sanseHun:" << hunCount << endl;
@@ -463,20 +496,86 @@ private:
 				}
 			}
 		}
-
 		// dumpSanSeHun
 		for (int i = 0; i < sizeof(m_dFinalSanSeHun) / sizeof(m_dFinalSanSeHun[0]); ++i) {
 			dumpTest(m_dFinalSanSeHun[i], "sanse"+itos(i));
 			//cout << "sanseKeShun size:" << m_dFinalSanSeHun[i].size() << endl;
 		}
+
+		// 将+刻、顺
+//		for (int j = 0; j <; ++j) {
+//
+//		}
 	}
 
+    void genFengJiangKeShun1() {
+		// jiang1 ke0
+
+        // 将c7/1 * c7/0刻
+		chooseFrom(m_dFengJiang, m_dFinalJiangFengHun[0], 1, m_dFengJiang.size());
+
+		/*
+		 * 将c7/1 * c7/1刻
+		 * 将c7/1 * c7/2刻
+		 * 将c7/1 * c7/3刻
+		 * 将c7/1 * c7/4刻
+		 * */
+		tbMap tmp;
+		chooseFrom(m_dFengJiang, tmp, 1, m_dFengJiang.size());
+		for (tbMap::iterator iteTmp = tmp.begin(); iteTmp != tmp.end(); ++iteTmp) {
+			Key k1 = iteTmp->first;
+			for (int i = 1; i <= 4; ++i) {
+				tbMap tmpKe;
+				chooseFrom(m_dFengKe, tmpKe, i, m_dFengKe.size());
+				for (tbMap::iterator iteTmpKe = tmpKe.begin(); iteTmpKe != tmpKe.end(); ++iteTmpKe) {
+					Key k2 = iteTmpKe->first;
+					Key key = KeyOpAdd(k1, k2);
+					if(valid(key)) {
+						m_dFinalJiangFengHun[0].insert(make_pair(key, getVal()));
+					}
+				}
+			}
+		}
+		// hun
+		int testCount = 0, collision = 0;
+		for (int hunCount = 0; hunCount < sizeof(m_dFinalJiangFengHun) / sizeof(m_dFinalJiangFengHun[0]) -1; ++hunCount) {
+			tbMap tmp = getMap(FengZi, hunCount, true);
+			for (tbMap::iterator ite = tmp.begin(); ite != tmp.end(); ++ite) {
+				tbVec keyVec = getKey(ite->first);
+				for (tbVec::iterator ite = keyVec.begin(); ite != keyVec.end(); ++ite) {
+					Key key = *ite;
+					m_dFinalJiangFengHun[hunCount+1].insert(make_pair(key, getVal()));
+					/*
+                     * 1个混儿,杠c7/1
+                     * 3个混儿,杠c7/2
+                     * 5个混儿,杠c7/3
+                     * 7个混儿,杠c7/4
+                     * 9个混儿,杠c7/5 X 14张手牌
+                     */
+                    if(hunCount+1 == 1) {
+						chooseFrom(m_dFengGang, m_dFinalJiangFengHun[hunCount+1], 1, m_dFengGang.size()); // 0000004 1个混可胡
+					} else if (hunCount+1 == 3){
+						chooseFrom(m_dFengGang, m_dFinalJiangFengHun[hunCount+1], 2, m_dFengGang.size());
+					} else if (hunCount+1 == 5){
+						chooseFrom(m_dFengGang, m_dFinalJiangFengHun[hunCount+1], 3, m_dFengGang.size());
+					} else if (hunCount+1 == 7){
+						chooseFrom(m_dFengGang, m_dFinalJiangFengHun[hunCount+1], 4, m_dFengGang.size());
+					}
+				}
+			}
+		}
+		cout << "feng jiang hun1 count:" << testCount << " collision count:" << collision << " map size:" << m_dFinalJiangFengHun[1].size() << endl;
+		// dumpJiangFengHun
+		for (int i = 0; i < sizeof(m_dFinalJiangFengHun) / sizeof(m_dFinalJiangFengHun[0]); ++i) {
+			dumpTest(m_dFinalJiangFengHun[i], "jiang_feng"+itos(i));
+			//cout << "fengKe size:" << m_dFinalFengHun[i].size() << endl;
+		}
+	}
 	void genFengMap() {
 		chooseFrom(m_dFengKe, m_dFinalFengHun[0], 1, m_dFengKe.size());
 		chooseFrom(m_dFengKe, m_dFinalFengHun[0], 2, m_dFengKe.size());
 		chooseFrom(m_dFengKe, m_dFinalFengHun[0], 3, m_dFengKe.size());
 		chooseFrom(m_dFengKe, m_dFinalFengHun[0], 4, m_dFengKe.size());
-
 		// hun
 		for (int hunCount = 0; hunCount < sizeof(m_dFinalFengHun) / sizeof(m_dFinalFengHun[0]) -1; ++hunCount) {
 			//cout << "fengHun:" << hunCount << endl;
@@ -489,12 +588,14 @@ private:
 				}
 			}
 		}
-
 		// dumpFengHun
-		for (int i = 0; i < sizeof(m_dFinalSanSeHun) / sizeof(m_dFinalSanSeHun[0]); ++i) {
+		for (int i = 0; i < sizeof(m_dFinalFengHun) / sizeof(m_dFinalFengHun[0]); ++i) {
 			dumpTest(m_dFinalFengHun[i], "feng"+itos(i));
 			//cout << "fengKe size:" << m_dFinalFengHun[i].size() << endl;
 		}
+
+		//
+        genFengJiangKeShun1();
 	}
 
 	void genMap() {
@@ -502,8 +603,8 @@ private:
 		genFengMap();
 	}
 
-	bool check(Key key, MjType tpe, int hunCount) {
-		tbMap &d = getMap(tpe, hunCount);
+	bool check(Key key, MjType tpe, int hunCount, bool hasJiang = false) {
+		tbMap &d = getMap(tpe, hunCount, hasJiang);
 		tbMap::iterator ite = d.find(key);
 		if (ite == d.end()) {
 			return false;
@@ -635,14 +736,17 @@ private:
 	}
 	bool checkHuHun(int *list, int len, int hunCount) {
 		int needHun = 0;
-		int arr[len];
+		int arr[9];	// 7 or 9
         memset(arr, 0, sizeof(arr));
 		for (int i = 0; i < coMax; ++i) {
 			int max = (i == coFengZi) ? 7 : 9;
-            memcpy(arr, list + 1 + max * (i+1), sizeof(arr));
+			if (i == coFengZi) {
+				memcpy(arr, list + 1, max * sizeof(int));
+			} else {
+				memcpy(arr, list + 1 + 7 + ((i-1) * max), max * sizeof(int));
+			}
 
-			Key key = getKey(arr, max);
-			int least = getLeastHunCount(key);
+			int least = getLeastHunCount(MjColorType(i), arr, max);
 			needHun += least;
 			if (needHun > hunCount) {
 				return false;
@@ -655,12 +759,45 @@ private:
 
 		return false;
 	}
-	int getLeastHunCount(Key key) {
-		return 0;
+	int getLeastHunCount(MjColorType co, int *list, int len) {
+		MjType tpe;
+		bool checkJiang = false;
+        int jiangCount = 0;
+		int arrJiang[7];
+		memset(arrJiang, 0, sizeof(arrJiang));
+		if (co == coFengZi) {
+			tpe = FengZi;
+			jiangCount = splitFengJiang(list, len, arrJiang);
+		} else {
+			tpe = SanSe;
+			jiangCount = splitJiang(list, list + len, arrJiang);
+		}
+
+		// 如果拆解出来的牌里没有将,
+		// 则从带将的表里开始检查是否胡
+		checkJiang = jiangCount == 0;
+		Key key = getKey(list, len);
+		for (int i = 1; i < MAX_HUN_COUNT + 1; ++i) {
+			// 先检查带将牌
+			if (check(key, tpe, i, checkJiang)) {
+				return i;
+			}
+		}
+
+		// 检查完带将牌不能胡，则检查不带将牌
+		for (int i = 1; i < MAX_HUN_COUNT + 1; ++i) {
+			// 先检查带将牌
+			if (check(key, tpe, i)) {
+				return i;
+			}
+		}
+		return MAX_HUN_COUNT+1;
 	}
 private:
 	tbMap m_dFinalSanSeHun[MAX_HUN_COUNT+1];    // 最终存放带混三色数据
 	tbMap m_dFinalFengHun[MAX_HUN_COUNT+1];     // 最终存放带混风字数据
+	tbMap m_dFinalJiangSanSeHun[MAX_HUN_COUNT+1];    // 最终存放带将、混三色数据
+	tbMap m_dFinalJiangFengHun[MAX_HUN_COUNT+1];     // 最终存放带将、混风字数据
 
 	// tmpData
 	tbVec m_dSanSeJiang;
@@ -668,6 +805,7 @@ private:
 	tbVec m_dSanSeShun;     // 用于生成三色牌Shun c(n/m)
 	tbVec m_dFengJiang;
 	tbVec m_dFengKe;        // 用于生成风牌Ke c(n/m)
+	tbVec m_dFengGang;      // 用于生成风牌Gang c(n/m)
 };
 
 void print_cards(int* cards) {
@@ -731,6 +869,7 @@ void benchmark(MjMapTbV3& tb) {
 			memset(cards, 0, MAX_MJ_CODEARRAY);
 			for (int j = i * 14; j < i * 14 + 14 - hunCount; j++)
 				++cards[allCards[j]];
+            cards[0] = hunCount;
 		}
 	}
 	gettimeofday(&end, NULL);
@@ -743,7 +882,7 @@ void benchmark(MjMapTbV3& tb) {
 	for (int n = 0; n < total; ++n) {
 		//sleep(1);
 //        hu += tb.checkHu(source + n * 43, 43, hunCount);
-		if (tb.checkHu(source + n * MAX_MJ_CODEARRAY, MAX_MJ_CODEARRAY, hunCount)) {
+		if (tb.checkHu(source + n * MAX_MJ_CODEARRAY, MAX_MJ_CODEARRAY)) {
 //            print_cards(source + n * MAX_MJ_CODEARRAY);
 			++hu;
 		} else {
@@ -755,24 +894,23 @@ void benchmark(MjMapTbV3& tb) {
 	cout << "check:" << total/10000 << "w hun:" << hunCount << " hu:" << hu << " cost:" << timeUse << "ms" << endl;
 	cout << "====benchmark end" << endl;
 }
-void check(MjMapTbV3& t) {
+void checkV1(MjMapTbV3& t) {
 	cout << "====begin checkV1 ..." << endl;
 	// 原手牌格式
 	int cards[] = {
-			0,                            // 混个数
+			3,                            // 混个数
 			1, 0, 0, 0, 0, 0, 1,          // 风字牌1-7
-			0, 0, 0, 0, 0, 0, 0, 0, 0,    // 万8-16
-			0, 0, 0, 0, 0, 0, 3, 0, 0,    // 条17-25
+			0, 0, 0, 0, 0, 0, 0, 0, 3,    // 万8-16
+			0, 0, 0, 0, 0, 0, 3, 0, 3,    // 条17-25
 			0, 0, 0, 0, 0, 0, 0, 0, 0,    // 筒26-34
 			0, 0, 0, 0, 0, 0, 0, 0        // 花35-42
 	};
 
-	int hunCount = 4;
 	double timeUse = 0;
 	struct timeval start;
 	struct timeval end;
 	gettimeofday(&start, NULL);
-	bool hu = t.checkHu(cards, sizeof(cards) / sizeof(cards[0]), hunCount);
+	bool hu = t.checkHu(cards, sizeof(cards) / sizeof(cards[0]));
 	gettimeofday(&end, NULL);
 	timeUse = (end.tv_sec - start.tv_sec) * 1000 * 1000 + (end.tv_usec - start.tv_usec);
 	cout << "====checkV1 hu?:" << hu << " cost:" << timeUse << "us" << endl;
@@ -798,7 +936,8 @@ void checkV2(MjMapTbV3 &tb) {
 	int hu = 0;
 	int hunCount = 8;
 	for (int i = 0; i < sizeof(cards) / sizeof(cards[0]) / 43; ++i) {
-		if (tb.checkHu(cards + i*43, 35, hunCount)) {
+        cards[0] = hunCount;
+		if (tb.checkHu(cards + i*43, 35)) {
 			print_cards(cards + i *43);
 			++hu;
 		}
@@ -846,7 +985,7 @@ void checkV3(MjMapTbV3 &t) {
 			tmp = key[i];
 			list[i] = atoi(tmp.c_str());
 		}
-		if(t.checkHu(list, 43, hunCount)) {
+		if(t.checkHu(list, 43)) {
 			++hu;
 		} else {
 			//print_cards(list);
@@ -860,9 +999,14 @@ void checkV3(MjMapTbV3 &t) {
 	cout << "checkV3 hu?:" << hu << " cost:" << timeUse << "ms" << endl;
 }
 int main(int argc, char **argv) {
-	MjMapTbV2 t;
+	MjMapTbV3 t;
+	vector<int> vec = t.getKey(23);
+	for (vector<int>::iterator ite = vec.begin(); ite != vec.end(); ++ite) {
+		cout << *ite << endl;
+	}
+//	return 0;
 	t.load();
-	check(t);
+	checkV1(t);
 //    checkV2(t);
 //    checkV3(t);
 
@@ -876,7 +1020,7 @@ int main(int argc, char **argv) {
 	int tpe = atoi(argv[1]);
 	if (tpe == 1) {
 		// gen map
-		MjMapTbV2 mt;
+		MjMapTbV3 mt;
 		mt.load();
 		int tmp;
 		cin >> tmp;
